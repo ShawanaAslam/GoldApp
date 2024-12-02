d3d37ee52ddaae534fb0c84d0e361482e6fc3e8c
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-02 05:55:00 -0800

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:goldapp/src/Home_Views/Home_View/gold-HomeView.dart';
 import 'package:goldapp/src/view/Starting_Views/splash_View/splash_View.dart';
 import 'package:goldapp/src/view/auth_View/signUp_View/signUp_View.dart';
 import 'package:goldapp/src/view/selection_View/selection_View.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -46,8 +47,8 @@ class MyApp extends StatelessWidget {
               //GoldShop()
               // SignupView()
               // SelectionView()
-              SplashView() //const MyHomePage(title: 'Flutter Demo Home Page'),
-
+             // SplashView() //const MyHomePage(title: 'Flutter Demo Home Page'),
+              UrlScreen()
           );
         }
     );
@@ -55,90 +56,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class UrlScreen extends StatefulWidget {
+  const UrlScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<UrlScreen> createState() => _UrlScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _UrlScreenState extends State<UrlScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child:
+       InkWell(
+         onTap: ()async{
+           // Link create for whastapp
+           final whatsappUrl=Uri.parse("https://wa.me/+923346985327");
+           final githubUrl=Uri.parse("https://github.com/Siddique722");
+           final liknedin=Uri.parse("www.linkedin.com/in/siddique-qureshi-22bb78233");
+           await launchUrl(whatsappUrl);
+         },
+         child:  Text('Contact On WhatsApp'),
+       )),
     );
   }
 }
