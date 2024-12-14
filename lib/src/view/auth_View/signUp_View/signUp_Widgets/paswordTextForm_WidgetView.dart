fa044d6402a2a5b1929f196e5be09a981906f22d
Shawana Aslam <shawanaaslam412@gmail.com>
2024-12-14 18:32:55 +0500

    code update

--- lib/src/view/auth_View/signUp_View/signUp_Widgets/paswordTextForm_WidgetView.dart
+++ lib/src/view/auth_View/signUp_View/signUp_Widgets/paswordTextForm_WidgetView.dart
@@ -120,74 +120,72 @@ class _PaswordTextFormState extends State<PaswordTextForm> {
 
 
 //////-----------------------------------------------------------------
-///
-//  Padding(
-//   padding: EdgeInsets.only(left: 30, right: 30),
-//   child:
-//   TextFormField(
-//     controller: widget.controller,
-//     // true / false
-//     validator: (value) {
-//       if (value == null || value.isEmpty) {
-//         return 'Please enter your ${widget.hnttxt}';
-//       }
-//       // if(hnttxt =='User name' && !RegExp(r'^[a-zA-z]').hasMatch(value))
-//       // {
-//       //   return 'Username must start with a letter';
-//       // }
-//       // if (hnttxt == 'Email' && !RegExp(r'^[\w-\.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,}$').hasMatch(value)) {
-//       //   return 'Please enter a valid email address';
-//       // }
-//       // if(hnttxt =='Enter Email' && !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4$}').hasMatch(value))
-//       // {
-//       //   return 'Please enter a valid email address';
-//       // }
-//       if (widget.hnttxt == 'Enter Pasword' && value.length < 8) {
-//         return 'Pasword must be at least 8 characters long';
-//       }
-//       if (widget.hnttxt == 'Enter Password' &&
-//           !RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
-//               .hasMatch(value)) {
-//         return 'Password must contain letters, numbers, and special characters';
-//       }
 
-//       // if(hnttxt == 'Enter Pasword' && !RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$').hasMatch(value))
-//       // {
-//       //   return 'Pasword must contain letters,numbers, and special characters';
-//       // }
-//       return null;
-//     },
-//     obscureText: secure,
-//     obscuringCharacter: '*',
-//     decoration: InputDecoration(
-//         prefixIcon: widget.prefixicn ?? null,
-//         suffixIcon: IconButton(
-//           onPressed: () {
-//             if (secure == false) {
-//               setState(() {
-//                 secure = true;
-//               });
-//             } else if (secure == true) {
-//               setState(() {
-//                 secure = false;
-//               });
-//             }
-//           },
-//           icon: secure
-//               ? Icon(Icons.remove_red_eye_outlined)
-//               : Icon(Icons.remove_red_eye),
-//         ),
-//         fillColor: widget.clr,
-//         filled: true,
-//         hintText: widget.hnttxt,
-//         hintStyle: TextStyle(color: Colors.black, fontSize: 14),
-//         border: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(15),
-//         ),
-//         errorBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(15),
-//         )),
-//   ),
-// );
+class PaswordTextformfield extends StatefulWidget {
+  TextEditingController controller;
+  String hnttxt;
+  Color clr;
+  Widget? prefixicn;
 
+  PaswordTextformfield({super.key,
+    required this.controller,required this.hnttxt,
+    this.clr=Colors.white,this.prefixicn});
 
+  @override
+  State<PaswordTextformfield> createState() => _PaswordTextformfieldState();
+}
+
+class _PaswordTextformfieldState extends State<PaswordTextformfield> {
+  bool secure=true;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Padding(
+        padding:  EdgeInsets.only(left: 30,right: 30),
+        child: TextFormField(
+          obscureText: secure,
+          obscuringCharacter: '*',
+          controller: widget.controller,
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock,),
+              suffixIcon: IconButton(onPressed: (){
+                if(secure==false){
+                  setState((){
+                    secure=true;
+                  });
+                }
+                else if(secure==true){
+                  setState(() {
+                    secure=false;
+                  });
+                }
+              },icon: secure?Icon(Icons.remove_red_eye_outlined):Icon(Icons.remove_red_eye),),
+              fillColor: widget.clr,
+              filled: true,
+              labelText:widget.hnttxt,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              hintStyle: TextStyle(color: Colors.amber, fontSize: 14),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber, ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber, ),
+                borderRadius: BorderRadius.circular(10),// Border when focused
+              ),
+              errorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber, ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(10),
+              )
+          ),
+        ),
+      );
+  }
+}
