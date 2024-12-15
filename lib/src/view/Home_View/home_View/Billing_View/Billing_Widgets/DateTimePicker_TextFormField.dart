ca6d760aecefac62093c57552a299f9579a33c0b
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-15 05:11:13 -0800

    up

--- lib/src/view/Home_View/home_View/Billing_View/Billing_Widgets/DateTimePicker_TextFormField.dart
+++ lib/src/view/Home_View/home_View/Billing_View/Billing_Widgets/DateTimePicker_TextFormField.dart
@@ -2,84 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../../../../Constants/colors.dart';
+
 class DatePickerTextformfield extends StatefulWidget {
   TextEditingController controller;
   String hnttxt;
   Color clr;
 
-  DatePickerTextformfield({super.key,required this.controller,
+  DatePickerTextformfield({
+    super.key,
+    required this.controller,
     required this.hnttxt,
-    this.clr=Colors.white,});
+    this.clr = Colors.white,
+  });
 
   @override
-  State<DatePickerTextformfield> createState() => _DatePickerTextformfieldState();
+  State<DatePickerTextformfield> createState() =>
+      _DatePickerTextformfieldState();
 }
 
 class _DatePickerTextformfieldState extends State<DatePickerTextformfield> {
   DateTime? selectedDate;
 
-  Future<void>selectdate(BuildContext context) async
-  {
+  Future<void> selectdate(BuildContext context) async {
     print("function starting");
-    DateTime? picked= await showDatePicker(
+    DateTime? picked = await showDatePicker(
         context: context,
         firstDate: DateTime(2000),
         lastDate: DateTime(2028),
-        initialDate: DateTime.now()
-    );
-    if(picked!=null && picked!=selectedDate)
-    {
+        initialDate: DateTime.now());
+    if (picked != null && picked != selectedDate) {
       setState(() {
-        selectedDate=picked;
-
+        selectedDate = picked;
+        widget.controller.text = selectedDate.toString();
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-      Padding(
-        padding:  EdgeInsets.only(left: 30,right: 30),
-        child: TextFormField(
-          controller: widget.controller,
-          cursorColor: AppColors.secondryColor,
-          style: TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-              suffixIcon: IconButton(onPressed: (){
+    return Padding(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      child: TextFormField(
+        controller: widget.controller,
+        cursorColor: AppColors.secondryColor,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            suffixIcon: IconButton(
+              onPressed: () {
                 selectdate(context);
-              },icon: Icon(Icons.calendar_month_rounded,
-color: AppColors.secondryColor,
-              ),),
-
-              fillColor: widget.clr,
-              filled: true,
-              hintText:widget.hnttxt,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+              },
+              icon: Icon(
+                Icons.calendar_month_rounded,
+                color: AppColors.secondryColor,
               ),
-              errorStyle: TextStyle(color: AppColors.secondryColor),
-              hintStyle: TextStyle(color: Colors.amber, fontSize: 14),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: AppColors.primryColor, ),
-                borderRadius: BorderRadius.circular(10),
+            ),
+            fillColor: widget.clr,
+            filled: true,
+            hintText: widget.hnttxt,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            errorStyle: TextStyle(color: AppColors.secondryColor),
+            hintStyle: TextStyle(color: Colors.amber, fontSize: 14),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.primryColor,
               ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: AppColors.primryColor, ),
-                borderRadius: BorderRadius.circular(10),// Border when focused
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.primryColor,
               ),
-              errorBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: AppColors.primryColor, ),
-                borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(10), // Border when focused
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.primryColor,
               ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderSide: BorderSide(color:AppColors.primryColor),
-                borderRadius: BorderRadius.circular(10),
-              )
-          ),
-        ),
-      );
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primryColor),
+              borderRadius: BorderRadius.circular(10),
+            )),
+      ),
+    );
   }
 }
-
-
--- lib/src/view/Home_View/home_View/Billing_View/billing_homeView.dart
+++ lib/src/view/Home_View/home_View/Billing_View/billing_homeView.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:goldapp/src/view/Home_View/home_View/Billing_View/billing_Controller.dart';
 import 'package:goldapp/src/view/Home_View/home_View/gold%20HomeViewController.dart';
 
 import '../../../../Constants/colors.dart';
 import '../../../../common_widgets/reusable_simpletext.dart';
 import '../../../../common_widgets/reusable_text.dart';
-// <<<<<<< HEAD
-// =======
-// import '../../../../common_widgets/simpleTextForm_widget.dart';
-// import 'Billing_Widgets/DateTimePicker_TextFormField.dart';
-// import 'Billing_Widgets/billing_TextFormField.dart';
-// >>>>>>> f5ec36a119ecd176767180a06dfd522b0f7e2848
+import '../../../../common_widgets/simpleTextForm_widget.dart';
+import 'Billing_Widgets/DateTimePicker_TextFormField.dart';
+import 'Billing_Widgets/billing_TextFormField.dart';
 
 class BillingHomeView extends StatefulWidget {
   String goldPrice;
@@ -29,7 +27,6 @@ class BillingHomeView extends StatefulWidget {
       required this.pointsQuantity,
       required this.totalPrice});
 
-
   @override
   State<BillingHomeView> createState() => _BillingHomeViewState();
 }
@@ -41,7 +38,8 @@ class _BillingHomeViewState extends State<BillingHomeView> {
     super.initState();
     obj.refresh();
   }
-  GoldShopController obj = Get.put(GoldShopController());
+
+  BillingController obj = Get.put(BillingController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,58 +53,33 @@ class _BillingHomeViewState extends State<BillingHomeView> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
         title: MyText(
-          txt: 'hhh',
+          txt: 'Billing Preview',
           font: 22,
           txtclr: AppColors.primryColor,
         ),
         centerTitle: true,
       ),
-// <<<<<<< HEAD
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             // SimpleText(controller: ),
-
-
-
-
-//               MyText(
-//                 txt: 'Gold Price : ${widget.goldPrice}',
-//                 txtclr: AppColors.secondryColor,
-//               ),
-//               MyText(
-//                   txt: 'Tola Quantity : ${widget.tolaQuantity}',
-//                   txtclr: AppColors.secondryColor),
-//               MyText(
-//                   txt: 'Grams Quantity : ${widget.gramsQuantity}',
-//                   txtclr: AppColors.secondryColor),
-//               MyText(
-//                   txt: 'Rati Quantity : ${widget.ratiQuantity}',
-//                   txtclr: AppColors.secondryColor),
-//               MyText(
-//                   txt: 'Tola Price : ${widget.totalPrice}',
-//                   txtclr: AppColors.secondryColor),
-// =======
       body: Padding(
         //padding: const EdgeInsets.only(bottom: 18,right: 20,left: 20),
-       padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
               Padding(
-                padding: const EdgeInsets.only(left: 130,),
-                child:
-          DatePickerTextformfield(
+                padding: const EdgeInsets.only(
+                  left: 130,
+                ),
+                child: DatePickerTextformfield(
                   controller: obj.DateController,
-                hnttxt: 'Enter Date',
-                clr: AppColors.primryColor,),
+                  hnttxt: 'Enter Date',
+                  clr: AppColors.primryColor,
+                ),
               ),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               BillingTextFormField(
                   // prefixicn: Icon(
                   //   Icons.email,
@@ -114,17 +87,19 @@ class _BillingHomeViewState extends State<BillingHomeView> {
                   // ),
                   controller: obj.clientController,
                   hnttxt: "Enter Client Name"),
-
-              SizedBox(height: 5,),
+              SizedBox(
+                height: 5,
+              ),
               BillingTextFormField(
-                // prefixicn: Icon(
-                //   Icons.email,
-                //   color: Colors.amber,
-                // ),
+                  // prefixicn: Icon(
+                  //   Icons.email,
+                  //   color: Colors.amber,
+                  // ),
                   controller: obj.productController,
                   hnttxt: "Enter Product Name"),
-
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Padding(
                 padding: const EdgeInsets.only(left: 40),
                 child: Container(
@@ -132,7 +107,6 @@ class _BillingHomeViewState extends State<BillingHomeView> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-
                       MyText(
                         txt: 'Gold Price : ${widget.goldPrice}',
                         txtclr: AppColors.secondryColor,
@@ -153,16 +127,10 @@ class _BillingHomeViewState extends State<BillingHomeView> {
                   ),
                 ),
               )
-
-
->>>>>>> f5ec36a119ecd176767180a06dfd522b0f7e2848
-
             ],
           ),
         ),
-      
+      ),
     );
- 
- );
   }
 }
